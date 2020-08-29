689f1f98d6253fcb81f9ccb5b90e49b865b8874a
khai96_ <hvksmr1996@gmail.com>
2020-08-29 10:03:29 +0700

    Use generated types

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,15 @@
 import * as ulib from 'utility-types'
-import * as tuple from 'typescript-tuple'
+import * as generated from './generated'
+
+/**
+ * Largest permitted ceiling value
+ */
+export type RangeMaxCeiling = generated.RangeMaxCeiling
+
+/**
+ * Permitted bound
+ */
+export type RangeBound = generated.RangeBound
 
 /**
  * Create a union from tuple's element
@@ -13,29 +23,18 @@ export type FromTuple<Tuple extends any[]> = utils.FromTuple<Tuple>
  * Create a union of range from 0 to `Max`, excluding `Max`
  * @example `RangeZero<5>` → `0 | 1 | 2 | 3 | 4`
  */
-export type RangeZero<Max extends number> = utils.RangeZero<Max>
+export type RangeZero<Ceiling extends RangeBound> = generated.RangeSet[Ceiling]
 
 /**
  * Create a union of range from `Min` to `Max`, including `Min`, excluding `Max`
  * @example `Range<3, 7>` → `3 | 4 | 5 | 6`
  */
-export type Range<Min extends number, Max extends number> = utils.Range<Min, Max>
+export type Range<Min extends RangeBound, Max extends RangeBound> = utils.Range<Min, Max>
 
 export namespace utils {
   export type FromTuple<Tuple extends any[]> =
     Tuple extends Iterable<infer X> ? X : never
 
-  export type RangeZero<
-    Ceiling extends number,
-    Holder extends number = never,
-    Counter extends any[] = []
-  > = {
-    matched: Holder,
-    unmatched: RangeZero<Ceiling, Holder | Counter['length'], tuple.Prepend<Counter, any>>
-  }[
-    Counter['length'] extends Ceiling ? 'matched' : 'unmatched'
-  ]
-
-  export type Range<Floor extends number, Ceiling extends number> =
+  export type Range<Floor extends RangeBound, Ceiling extends RangeBound> =
     ulib.SetDifference<RangeZero<Ceiling>, RangeZero<Floor>>
 }
